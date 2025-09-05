a938c80026a464429f1808c175488272e165a604
Your Name <your-email@example.com>
2025-09-05 21:16:00 +0300

    add login component

--- src/app/Core/auth/Services/auth.service.ts
+++ src/app/Core/auth/Services/auth.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private _HttpClient:HttpClient) { }
+
+  login(data:any):Observable<any>{
+    console.log('Service called: users/login');
+    return this._HttpClient.post('users/login',data)
+  }
+     
 }
+
+
--- src/app/Core/interceptors/global.interceptor.ts
+++ src/app/Core/interceptors/global.interceptor.ts
@@ -1,10 +1,12 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const globalInterceptor: HttpInterceptorFn = (req, next) => {
+  
   const baseUrl: string = 'https://154.41.228.234:3000/api/v0/portal/';
 
   const updated = req.clone({
     url: baseUrl + req.url,
   });
+   console.log('Request URL:', updated.url);
   return next(updated);
 };
