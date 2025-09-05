c2291318e1b0c362f2fa3d7f46a0f9c5a6f9707b
Mohamed Osama Menisy <116950544+mohamedmenisy@users.noreply.github.com>
2025-09-06 00:39:44 +0300

    update project

--- src/app/Core/auth/Components/login/login.component.ts
+++ src/app/Core/auth/Components/login/login.component.ts
@@ -1,13 +1,5 @@
 import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroupDirective,
-  NgForm,
-  Validators,
-  FormsModule,
-  ReactiveFormsModule,
-  FormGroup,
-} from '@angular/forms';
+import {FormControl,Validators,FormsModule, ReactiveFormsModule,FormGroup} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 import {NgIf} from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
@@ -15,20 +7,11 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { AuthService } from '../../Services/auth.service';
-import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ FormsModule,
-    MatIconModule,
-    MatButtonModule,
-  ReactiveFormsModule,
-// HttpClientModule,
-MatInputModule,
-MatFormFieldModule,
-CommonModule,
-NgIf
+  imports: [ FormsModule,MatIconModule,MatButtonModule,ReactiveFormsModule,MatInputModule,MatFormFieldModule,CommonModule,NgIf
 ],
  providers: [AuthService],
   templateUrl: './login.component.html',
@@ -36,27 +19,25 @@ NgIf
 })
 
 export class LoginComponent {
-
- 
+  constructor(private _AuthService:AuthService){}
+  PasswordPattent:RegExp =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{1,10}$/;
+  passwordError:string="Password must contain uppercase, lowercase, number, symbol (max 10 chars)"
  hide = true;
- constructor(private _AuthService:AuthService){}
   matcher = new ErrorStateMatcher();
   value = 'Clear me';
-  lohin(data:FormGroup){
-this._AuthService.login(data.value).subscribe({
-  next:(res)=>{
-console.log(res);
-localStorage.setItem('token',res.Token);
+  login(data:FormGroup){
+    this._AuthService.login(data.value).subscribe({
+      next:(res)=>{
+    console.log(res);
+    localStorage.setItem('token',res.Token);
+      }
+    });
   }
-})
-  }
-  
+
    loginForm = new FormGroup({
      email: new FormControl(null, [Validators.required, Validators.email]),
      password: new FormControl(null, [
-       Validators.required,
-       Validators.maxLength(20),
-       Validators.minLength(3),
+       Validators.required,Validators.pattern(this.PasswordPattent)
      ]),
    });
 
