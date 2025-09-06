1f5bb7f86d041c30cd076400e636a3a59d1bdf6e
Mohamed Osama Menisy <116950544+mohamedmenisy@users.noreply.github.com>
2025-09-06 10:48:34 +0300

    update login view

--- src/app/Core/auth/Components/login/login.component.ts
+++ src/app/Core/auth/Components/login/login.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import {FormControl,Validators,FormsModule, ReactiveFormsModule,FormGroup} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
-import {NgIf} from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { AuthService } from '../../Services/auth.service';
-import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ FormsModule,MatIconModule,MatButtonModule,ReactiveFormsModule,MatInputModule,MatFormFieldModule,CommonModule,NgIf
+  imports: [MatSnackBarModule, FormsModule,MatIconModule,MatButtonModule,ReactiveFormsModule,MatInputModule,MatFormFieldModule,RouterLink
 ],
  providers: [AuthService],
   templateUrl: './login.component.html',
@@ -19,20 +18,11 @@ import { CommonModule } from '@angular/common';
 })
 
 export class LoginComponent {
-  constructor(private _AuthService:AuthService){}
+  constructor(private _AuthService:AuthService,private _snackBar:MatSnackBar){}
   PasswordPattent:RegExp =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{1,10}$/;
   passwordError:string="Password must contain uppercase, lowercase, number, symbol (max 10 chars)"
- hide = true;
-  matcher = new ErrorStateMatcher();
-  value = 'Clear me';
-  login(data:FormGroup){
-    this._AuthService.login(data.value).subscribe({
-      next:(res)=>{
-    console.log(res);
-    localStorage.setItem('token',res.Token);
-      }
-    });
-  }
+   hide = true;
+
 
    loginForm = new FormGroup({
      email: new FormControl(null, [Validators.required, Validators.email]),
@@ -40,6 +30,28 @@ export class LoginComponent {
        Validators.required,Validators.pattern(this.PasswordPattent)
      ]),
    });
-
+  login(data:FormGroup){
+    this._AuthService.login(data.value).subscribe({
+      next:(res)=>{
+      localStorage.setItem('token',res.Token);
+       this._snackBar.open('Your password has been changed successfully 🎉',"", {
+          duration: 3000,
+          horizontalPosition: "end",
+          verticalPosition: "top",
+          panelClass: ['success-snackbar']
+        });
+      },
+      error:(err)=>{
+         this._snackBar.open("an error occurred","",
+                {
+                  duration: 3000,
+                  horizontalPosition: "end",
+                  verticalPosition: "top",
+                  panelClass: ['error-snackbar']
+                }
+        );
+      }
+    });
+  }
 
 }
