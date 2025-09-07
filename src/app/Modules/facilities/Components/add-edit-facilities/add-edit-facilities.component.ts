06c6e5178f051ff217edd7d1aff07fed793c99fc
Cipher-13 <aihab6087@gmail.com>
2025-09-08 00:23:16 +0300

    add & edit v3-snacked

--- src/app/Modules/facilities/Components/add-edit-facilities/add-edit-facilities.component.ts
+++ src/app/Modules/facilities/Components/add-edit-facilities/add-edit-facilities.component.ts
@@ -2,7 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FacilitesService } from '../../Services/facilites.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -19,41 +20,68 @@ import { ActivatedRoute } from '@angular/router';
 
 export class AddEditFacilitiesComponent {
 
-facilityId:number = 0
+  facilityId: number = 0
 
-constructor(private F1:FacilitesService,private act:ActivatedRoute){
-this.facilityId= act.snapshot.params['id']
-}
+  constructor(private F1: FacilitesService, private act: ActivatedRoute, private r: Router, private snack: MatSnackBar) {
+    this.facilityId = act.snapshot.params['id']
+  }
 
-FacitiyForm = new FormGroup({
-  name: new FormControl('', [Validators.required, Validators.minLength(10)])
-});
-
-
-Missions(d: FormGroup){ 
-
-  if(this.facilityId && this.facilityId > 0) { //Update Exist
- 
-  this.F1.Editor(this.facilityId,this.FacitiyForm.value).subscribe({
-  next:(Res)=> {console.log("Mission Accomplished !"+Res)},
-  error:(Res)=> {console.error("Aborting!"+Res)},
-  complete:()=> {},
-  
-})
-}
-
-else{  // Create New
-  
-  this.F1.Creator(this.FacitiyForm.value).subscribe({
- 
-  next:(Res)=> {console.log("Mission Accomplished !"+Res)},
-  error:(Res)=> {console.error("Aborting!"+Res)},
-  complete:()=> {},
-  
-})
-}
+  FacitiyForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(10)])
+  });
 
 
-}
+  Missions(d: FormGroup) {
+
+    if (this.facilityId && this.facilityId > 0) { //Update Exist
+
+      this.F1.Editor(this.facilityId, this.FacitiyForm.value).subscribe({
+        next: (Res) => {
+          console.log("Mission Accomplished !" + Res);
+          this.snack.open('Facility updated successfully!', 'Close', {
+            duration: 3000,
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+          });
+        },
+        error: (issue) => {
+          console.error("Aborting!" + issue);
+          this.snack.open('Update failed!', 'Close', {
+            duration: 3000,
+            panelClass: ['snackbar-error']
+          });
+        },
+        complete: () => { this.r.navigate(['/dashboard/facilities']); }, //Navigate to table
+
+      })
+    }
+
+    else {  // Create New
+
+      this.F1.Creator(this.FacitiyForm.value).subscribe({
+
+        next: (Res) => {
+          console.log("Mission Accomplished !" + Res);
+          this.snack.open('Facility created successfully!', 'Close', {
+            duration: 3000,
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+          });
+        },
+
+        error: (issue) => {
+          console.error("Aborting!" + issue);
+          this.snack.open('Creation failed!', 'Close', {
+            duration: 3000,
+            panelClass: ['snackbar-error']
+          });
+        },
+        complete: () => { this.r.navigate(['/dashboard/facilities']); }, //Navigate to table
+
+      })
+    }
+
+
+  }
 
 }
