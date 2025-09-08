7e5cf0dec2396ebb7303a6fe333495218f0dd136
Mohamed Osama Menisy <116950544+mohamedmenisy@users.noreply.github.com>
2025-09-08 23:58:50 +0300

    add-edit

--- src/app/Modules/rooms/Services/rooms.service.ts
+++ src/app/Modules/rooms/Services/rooms.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomsService {
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
+  createRoom(data:any):Observable<any>{
+    return this._http.post('rooms',data);
+  }
 }
