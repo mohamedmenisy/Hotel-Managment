475a7df1e0aa6c5b8fbc693d5836457038f6377f
SamehWally <samehwaly27@gmail.com>
2025-09-13 06:17:50 +0300

    feat: finalized room view

--- src/app/Modules/rooms/Components/room-view/room-view.component.ts
+++ src/app/Modules/rooms/Components/room-view/room-view.component.ts
@@ -1,27 +1,72 @@
-import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef, MatDialogContent } from '@angular/material/dialog';
-import { MatIcon } from "@angular/material/icon";
-import { DatePipe } from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { RoomsService } from '../../Services/rooms.service';
 
 @Component({
   selector: 'app-room-view',
   standalone: true,
-  imports: [MatIcon, DatePipe],
+  imports: [CommonModule],
   templateUrl: './room-view.component.html',
   styleUrl: './room-view.component.scss',
 })
-export class RoomViewComponent {
+export class RoomViewComponent implements OnInit, OnDestroy {
+  roomid: string = '';
+  room: any;
+  currentIndex = 0;
+  autoplayInterval: any;
+
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: any,
-    public dialogRef: MatDialogRef<RoomViewComponent>
+    private _active: ActivatedRoute,
+    private _roomsService: RoomsService
   ) {
-    console.log(data);
+    this.roomid = _active.snapshot.params['id'];
+    if (this.roomid) this.getRoomByid(this.roomid);
   }
 
-  onNoClick(): void {
-    this.dialogRef.close();
+  ngOnInit(): void {
+    this.startAutoplay();
   }
-  onNoClickdelete(): void {
-    this.dialogRef.close(true);
+
+  ngOnDestroy(): void {
+    clearInterval(this.autoplayInterval);
+  }
+
+  getRoomByid(id: string) {
+    this._roomsService.getRoomByid(id).subscribe({
+      next: async (res) => {
+        console.log(res);
+        this.room = res.data.room;
+      },
+    });
+  }
+
+  public get facilityNames(): string {
+    return this.room && this.room.facilities
+      ? this.room.facilities.map((f: any) => f.name).join(', ')
+      : '';
+  }
+
+  nextSlide() {
+    if (!this.room?.images?.length) return;
+    this.currentIndex = (this.currentIndex + 1) % this.room.images.length;
+  }
+
+  prevSlide() {
+    if (!this.room?.images?.length) return;
+    this.currentIndex =
+      (this.currentIndex - 1 + this.room.images.length) %
+      this.room.images.length;
+  }
+
+  startAutoplay() {
+    this.autoplayInterval = setInterval(() => {
+      this.nextSlide();
+    }, 4000);
+  }
+
+  openZoom(index: number) {
+    const img = this.room?.images?.[index];
+    if (img) window.open(img, '_blank');
   }
 }
