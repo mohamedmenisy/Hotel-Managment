ee2dbb8c5ad68a7a2791c671037c55866b69dadf
Cipher-13 <aihab6087@gmail.com>
2025-09-17 15:53:43 +0300

    details w rbna ystor

---
+++ src/app/master-layout/services/details.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RoomResponse } from '../../Modules/rooms/interfaces/room-details';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DetailsService {
+
+  constructor(private http :HttpClient) { }
+
+
+Detailed(id: string): Observable<RoomResponse> {
+  return this.http.get<RoomResponse>(`portal/rooms/${id}`);
+}
+
+
+
+
+}
